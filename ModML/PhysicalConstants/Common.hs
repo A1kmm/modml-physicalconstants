38a5ce92e28e76313dbd54f818510fd53246046c
Andrew Miller <ak.miller@auckland.ac.nz>
2010-10-15 18:04:45 +1300

    Initial version now building

--- ModML/PhysicalConstants/Common.hs
+++ ModML/PhysicalConstants/Common.hs
@@ -1,10 +1,12 @@
-module CommonConstants
+{-# LANGUAGE NoMonomorphismRestriction #-}
+module ModML.PhysicalConstants.Common
 where
 import qualified ModML.Units.UnitsDAEModel as U
+import ModML.Units.UnitsDAEOpAliases
 import ModML.Units.SIUnits
 
 speedOfLightInVacuum = U.realConstant (uMetre $*$ uSecond $**$(-1)) 2.99792458E8
-gravitationalConstant = U.realConstant (uMetre$**$3 $*$ uKilogram$**$(-1) uSecond$**$(-2) ) 6.6742867E-11
+gravitationalConstant = U.realConstant (uMetre$**$3 $*$ uKilogram$**$(-1) $*$ uSecond$**$(-2) ) 6.6742867E-11
 planckConstant = U.realConstant (uJoule $*$ uSecond) 6.6260689633E-34
 reducedPlanckConstant = planckConstant ./. (U.piX .*. U.realConstant U.dimensionless 2)
 magneticConstant = U.piX .*. (U.realConstant (uNewton $*$ uAmpere$**$(-2)) 4E-7)
@@ -27,14 +29,13 @@ fineStructureConstant = magneticConstant .*. elementaryCharge .*. elementaryChar
 hartreeEnergy = U.realConstant U.dimensionless 2 .*. rydbergConstant .*. planckConstant .*. speedOfLightInVacuum
 protonMass = U.realConstant uKilogram 1.67262163783E-27
 quantumOfCirculation = planckConstant ./. (U.realConstant U.dimensionless 2 .*. electronMass)
-rydbergConstant = fineStructureConstant.**.2 .*. electronMass .*. speedOfLightInVacuum ./. (U.realConstant U.dimensionless 2 .*. planckConstant)
-thomsonCrossSection = (U.realConstant U.dimensionless (8.0/3)) .*. U.piX .*. classicalElectronRadius.**.2
+rydbergConstant = fineStructureConstant.**.(U.realConstant U.dimensionless 2) .*. electronMass .*. speedOfLightInVacuum ./. (U.realConstant U.dimensionless 2 .*. planckConstant)
+thomsonCrossSection = (U.realConstant U.dimensionless (8.0/3)) .*. U.piX .*. classicalElectronRadius.**.(U.realConstant U.dimensionless 2)
 
 avogadrosNumber = U.realConstant (uMole $**$ (-1)) 6.0221417930E23
 boltzmanConstant = gasConstant ./. avogadrosNumber
 faradayConstant = avogadrosNumber .*. elementaryCharge
-loschmidtConstant = avogadrosNumber ./. molarVolumeIdealGas
 gasConstant = U.realConstant (uJoule $*$ (uMole $*$ uKelvin) $**$ (-1)) 8.31447215
 molarPlanckConstant = avogadrosNumber .*. planckConstant
-standardGravity = U.realConstant (uMetre .*. uSecond.**.(-2)) 9.80665
+standardGravity = U.realConstant (uMetre $*$ uSecond$**$(-2)) 9.80665
 standardAtmosphere = U.realConstant (uPascal) 101325
